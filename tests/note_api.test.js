9127874de262000e4dfd8d7abee4f638d862f760
Jere Tofferi <jere.tofferi@gmail.com>
2019-11-20 01:57:49 +0200

    Complete excercise 4.10

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -8,52 +8,40 @@ const api = supertest(app)
 
 const blogs = [
     {
-        _id: '5a422a851b54a676234d17f7',
         title: 'React patterns',
         author: 'Michael Chan',
         url: 'https://reactpatterns.com/',
         likes: 7,
-        __v: 0
     },
     {
-        _id: '5a422aa71b54a676234d17f8',
         title: 'Go To Statement Considered Harmful',
         author: 'Edsger W. Dijkstra',
         url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
         likes: 5,
-        __v: 0
     },
     {
-        _id: '5a422b3a1b54a676234d17f9',
         title: 'Canonical string reduction',
         author: 'Edsger W. Dijkstra',
         url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
         likes: 12,
-        __v: 0
     },
     {
-        _id: '5a422b891b54a676234d17fa',
         title: 'First class tests',
         author: 'Robert C. Martin',
         url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
         likes: 10,
-        __v: 0
     },
     {
-        _id: '5a422ba71b54a676234d17fb',
         title: 'TDD harms architecture',
         author: 'Robert C. Martin',
         url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
         likes: 0,
-        __v: 0
     },
     {
-        _id: '5a422bc61b54a676234d17fc',
         title: 'Type wars',
         author: 'Robert C. Martin',
         url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
         likes: 2,
-        __v: 0
     }
 ]
 
@@ -85,6 +73,27 @@ test('blog identifiers should be tagged as id', async () => {
     console.log(response.body[0])
 
     expect(response.body[0].id).toBeDefined()
+})
+
+test('post a blog to server', async () => {
+    const newBlog = {
+        title: 'Uus juttu',
+        author: 'joku',
+        url: 'yle.fi',
+        likes: 5,
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(b => b.title)
+
+    expect(response.body.length).toBe(blogs.length + 1)
+    expect(titles).toContain('Uus juttu')
 })
 
 afterAll(() => {
